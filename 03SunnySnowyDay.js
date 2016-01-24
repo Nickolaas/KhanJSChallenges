04bf32c1c6ddece4efea075e33f9ef143b1ee79e
Nickolaas <nick_decker5009@elcamino.edu>
2016-01-24 11:43:58 -0800

    03SunnySnowyDay.js

---
+++ 03SunnySnowyDay.js
@@ -0,0 +1,16 @@
+background(15, 87, 255);
+
+// The ground
+fill(128, 255, 0);
+rect(0, 300, 400, 100);
+
+// The sun
+fill(238, 255, 0);
+ellipse(80, 64, 100, 100);
+
+// The snowman
+fill(240, 231, 231);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+
