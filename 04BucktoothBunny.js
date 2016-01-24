c138d2b6ca3512eb09710bf389446ab48087c1e9
Nickolaas <nick_decker5009@elcamino.edu>
2016-01-24 11:44:15 -0800

    04BucktoothBunny

---
+++ 04BucktoothBunny.js
@@ -0,0 +1,20 @@
+var x = 60;
+var y = 101;
+
+
+
+ellipse(150, 70, 60, 120);  // left ear
+ellipse(240, 70, 60, 120);  // right ear
+
+ellipse(200, 170, 150, 150); // face
+
+fill(0, 0, 0);
+ellipse(170, 150, x, x); // left eye
+ellipse(230, 150, x, x); // right eye
+
+line(150, 200, 250, 200); // mouth
+
+noFill();
+rect(185, 200, 15, y);  // left tooth
+rect(200, 200, 15, y);  // right tooth
+
